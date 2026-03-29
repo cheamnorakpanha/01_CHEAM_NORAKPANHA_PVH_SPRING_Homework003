d3ed26cc9ce07036394e2ff38b686d3fe7209e97
Cheam Norakpanha <cheam.panha168@gmail.com>
2026-03-29 13:37:13 +0700

    fully implement venue

--- src/main/java/org/me/homework003/controller/VenueController.java
+++ src/main/java/org/me/homework003/controller/VenueController.java
@@ -1,13 +1,16 @@
 package org.me.homework003.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.me.homework003.model.entity.Venue;
+import org.me.homework003.model.request.VenueRequest;
+import org.me.homework003.model.response.ApiResponse;
 import org.me.homework003.service.VenueService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -16,9 +19,69 @@ import java.util.List;
 public class VenueController {
     private final VenueService venueService;
 
+    @Operation(summary = "Get all venues")
     @GetMapping
-    public List<Venue> getAllVenues(@RequestParam(defaultValue = "1") int page,
-                                    @RequestParam(defaultValue = "5") int size) {
-        return venueService.getAllVenues(page, size);
+    public ResponseEntity<ApiResponse<List<Venue>>> getAllVenues(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
+
+        ApiResponse<List<Venue>> response = ApiResponse.<List<Venue>>builder()
+                .timestamp(Instant.now())
+                .message("Retrieved venues successfully.")
+                .status(HttpStatus.OK)
+                .payload(venueService.getAllVenues(page, size))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Operation(summary = "Get all venues by Id")
+    @GetMapping("/{venue-id}")
+    public ResponseEntity<ApiResponse<List<Venue>>> getAllVenuesById(@PathVariable("venue-id") Long venueId) {
+        ApiResponse<List<Venue>> response = ApiResponse.<List<Venue>>builder()
+                .timestamp(Instant.now())
+                .message("Retrieved venues successfully.")
+                .status(HttpStatus.OK)
+                .payload(venueService.getAllVenuesById(venueId))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Operation(summary = "Create a new venue")
+    @PostMapping
+    public ResponseEntity<ApiResponse<List<Venue>>> createNewVenue(@RequestBody VenueRequest request) {
+        ApiResponse<List<Venue>> response = ApiResponse.<List<Venue>>builder()
+                .timestamp(Instant.now())
+                .message("Retrieved venues successfully.")
+                .status(HttpStatus.OK)
+                .payload(venueService.createNewVenue(request))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Operation(summary = "Delete venue by Id")
+    @DeleteMapping("/{venue-id}")
+    public ResponseEntity<ApiResponse<List<Venue>>> deleteVenueById(@PathVariable("venue-id") Long venueId) {
+        ApiResponse<List<Venue>> response = ApiResponse.<List<Venue>>builder()
+                .timestamp(Instant.now())
+                .message("Retrieved venues successfully.")
+                .status(HttpStatus.OK)
+                .payload(venueService.deleteVenueById(venueId))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Operation(summary = "Update venue by Id")
+    @PutMapping("/{venue-id}")
+    public ResponseEntity<ApiResponse<List<Venue>>> updateVenueById(@PathVariable("venue-id") Long venueId, @RequestBody VenueRequest request) {
+        ApiResponse<List<Venue>> response = ApiResponse.<List<Venue>>builder()
+                .timestamp(Instant.now())
+                .message("Retrieved venues successfully.")
+                .status(HttpStatus.OK)
+                .payload(venueService.updateVenueById(venueId, request))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/main/java/org/me/homework003/repository/VenueRepository.java
+++ src/main/java/org/me/homework003/repository/VenueRepository.java
@@ -1,10 +1,8 @@
 package org.me.homework003.repository;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.me.homework003.model.entity.Venue;
+import org.me.homework003.model.request.VenueRequest;
 
 import java.util.List;
 
@@ -16,7 +14,39 @@ public interface VenueRepository {
             @Result(property = "venueName", column = "venue_name"),
     })
     @Select("""
-        SELECT * FROM venues OFFSET #{offset} LIMIT #{size}
-    """)
+            SELECT * FROM venues OFFSET #{offset} LIMIT #{size}
+            """)
     List<Venue> getAllVenues(int offset, int size);
+
+    @ResultMap("venueMapper")
+    @Select("""
+            SELECT * FROM venues WHERE venue_id = #{venueId}
+            """)
+    List<Venue> getAllVenuesById(Long venueId);
+
+    @ResultMap("venueMapper")
+    @Select("""
+            INSERT INTO venues (venue_name, location)
+            VALUES (#{req.venueName}, #{req.location})
+            RETURNING *
+            """)
+    List<Venue> createNewVenue(@Param("req") VenueRequest request);
+
+    @ResultMap("venueMapper")
+    @Select("""
+            DELETE FROM venues
+            WHERE venue_id = #{venueId}
+            RETURNING *
+            """)
+    List<Venue> deleteVenueById(@Param("venueId") Long venueId);
+
+    @ResultMap("venueMapper")
+    @Select("""
+            UPDATE venues
+            SET venue_name = #{req.venueName},
+                location = #{req.location}
+            WHERE venue_id = #{venueId}
+            RETURNING *
+            """)
+    List<Venue> updateVenueById(@Param("venueId") Long venueId, @Param("req") VenueRequest request);
 }
